ec82bd3caedcf39e1b908916fbc2213de1189518
Dante Rognoni <131401219+Danrognoni@users.noreply.github.com>
2026-09-05 18:36:11 -0300

    api 2.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://julietphotograph-back.onrender.com',
-  uploadsUrl: 'http://localhost:8080'
+  uploadsUrl: 'https://julietphotograph-back.onrender.com'
 };
